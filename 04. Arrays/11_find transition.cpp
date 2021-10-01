43530e0e2cc9f330dc877b74447a86a0e3fef240
Astroakanksha <62433778+Astroakanksha24@users.noreply.github.com>
2021-10-01 12:11:21 +0530

    Added find transition solution.

--- 04. Arrays/11_find transition.cpp
+++ 04. Arrays/11_find transition.cpp
@@ -1,3 +1,12 @@
+//Problem discription
+//Given a sorted array containing only numbers 0 and 1, the task is to find the transition point efficiently. The transition point is the point where “0” ends and “1” begins.
+// Input: 0 0 0 1 1
+// Output: 3
+// Explanation: Index of first 1 is 3
+
+
+
+
 // { Driver Code Starts
 #include <bits/stdc++.h>
 using namespace std;
@@ -25,4 +34,32 @@ int main()
 int transitionPoint(int arr[], int n)
 {
     // code here
+    // Initialise lower and upper bounnds
+    int lb = 0, ub = n-1;
+ 
+    // Perform Binary search
+    while (lb <= ub)
+    {
+        // Find mid
+        int mid = (lb+ub)/2;
+ 
+        // update lower_bound if mid contains 0
+        if (arr[mid] == 0)
+            lb = mid+1;
+ 
+        // If mid contains 1
+        else if (arr[mid] == 1)
+        {
+            // Check if it is the left most 1
+            // Return mid, if yes
+            if (mid == 0
+                    || (mid > 0 &&
+                       arr[mid - 1] == 0))
+                return mid;
+ 
+            // Else update upper_bound
+            ub = mid-1;
+        }
+    }
+    return -1;
 }
