3a6e124749c61357451214b0faf96932362ee701
Hitesh Vishnoi <67196487+hiteshv01@users.noreply.github.com>
2021-10-01 12:17:38 +0530

    Create Solution Q1.cpp

---
+++ 02. Bit Magic/Solution Q1.cpp
@@ -0,0 +1,32 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int setBits(int N)
+    {
+        // Write Your Code here
+       return __builtin_popcount(N) ;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
