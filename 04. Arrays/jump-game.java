c231360e18d214c01a3cb80f716825e24e2f3749
Subham Das <das.jishu25@gmail.com>
2021-10-01 15:31:15 +0530

    Create jump-game.java

---
+++ 04. Arrays/jump-game.java
@@ -0,0 +1,42 @@
+/*
+# JUMP GAME
+
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Determine if you are able to reach the last index.
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+Constraints:
+
+1 <= nums.length <= 3 * 10^4
+0 <= nums[i][j] <= 10^5
+*/
+
+// SOLUTION:
+
+class Solution {
+   public boolean canJump(int[] A) {
+    int max = 0;
+    for(int i=0;i<A.length;i++){
+        if(i>max) {return false;}
+        max = Math.max(A[i]+i,max);
+    }
+    return true;
+  }
+}
+
+// Time complexity: O(N)
+// Space complexity: O(1)
