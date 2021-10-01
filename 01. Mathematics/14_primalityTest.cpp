af8e70222f2fbffec16eb69fd7821afd1370a9db
Adarsh <adarsh333reddy@gmail.com>
2021-10-02 00:43:31 +0530

    added primality test code

--- 01. Mathematics/14_primalityTest.cpp
+++ 01. Mathematics/14_primalityTest.cpp
@@ -12,6 +12,12 @@ class Solution
 public:
     bool isPrime(int N)
     {
+        for(int i=2;i<=sqrt(N);i++)
+        {
+            if(N%i==0)
+             return false;
+        }
+        return true;
         //Your code here
     }
 };
