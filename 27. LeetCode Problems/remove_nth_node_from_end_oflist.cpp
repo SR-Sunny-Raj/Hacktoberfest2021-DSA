d5aa4a4d098bedbd95b22f699c046b16db3b21fb
Rajarshi1001 <rajarshi20@iitk.ac.in>
2021-10-02 23:09:57 +0530

    Create remove_nth_node_from_end_oflist.cpp

--- 27. LeetCode Problems/remove_nth_node_from_end_oflist.cpp
+++ 27. LeetCode Problems/remove_nth_node_from_end_oflist.cpp
@@ -1,12 +1,14 @@
 // Given the head of a linked list, 
 // remove the nth node from the end of the list and return its head.
 
-struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
+class Solution {
+public:
+    struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
     struct ListNode*p=(struct ListNode*)malloc(sizeof(struct ListNode));
     p->next=head;
-   struct ListNode* s=p;
+    struct ListNode* s=p;
     struct ListNode* f=p;
-   for(int i=1; i<=n; i++)
+    for(int i=1; i<=n; i++)
         f=f->next;
      while(f->next!=NULL)
     {
@@ -16,3 +18,4 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
     s->next=s->next->next;
     return p->next;
 }
+};
