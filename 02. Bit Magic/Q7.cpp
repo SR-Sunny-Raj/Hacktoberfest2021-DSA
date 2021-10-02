391b6ad229369607b864a86d52b4c40a61f44c07
Abhijit25Mishra <69361857+Abhijit25Mishra@users.noreply.github.com>
2021-10-02 10:21:44 +0530

    Delete Q7.cpp

--- 02. Bit Magic/Q7.cpp
+++
@@ -1,7 +0,0 @@
-//  Function to check if x is power of 2
-bool isPowerOfTwo(int x)
-{
-    // First x in the below expression is
-    // for  the case when x is 0
-    return x && (!(x & (x - 1)));
-}
