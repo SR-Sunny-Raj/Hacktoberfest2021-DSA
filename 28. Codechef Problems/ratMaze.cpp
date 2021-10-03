cedf89af50bf86e707c6ca980884d5327b4a298a
Gupta-Nikhil85 <nikhilgupta8800@gmail.com>
2021-10-03 16:57:10 +0530

    Question added

--- 28. Codechef Problems/ratMaze.cpp
+++ 28. Codechef Problems/ratMaze.cpp
@@ -1,3 +1,12 @@
+// Story: You have to find a path through which the rat move from the starting position (0,0) to the final position where cheese is (n,n). List the total no of possible paths which the rat can take to reach the cheese.
+
+// Input
+// A file whose first line contains the size of the square maze The next n lines contain n binary values separated by space. 0 denotes an open cell while 1 denotes a blocked cell. You have to find paths which contain only cells which are open, and finally print the total no of such unique paths. Retracing the 1 or more cells back and forth is not considered a new path. The set of all cells covered in a single path should be unique from other paths
+
+// Output
+// The output should be the total no of paths possible on the first line
+
+
 #include <bits/stdc++.h>
 #define vvi vector<vector<int>>
 using namespace std;
