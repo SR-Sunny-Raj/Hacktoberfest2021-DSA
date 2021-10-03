03f95e36c0f48a0f6a5dfda9fa770bc62987fc97
DaniyalMustafa <63039329+DaniyalMustafa@users.noreply.github.com>
2021-10-04 04:56:48 +0530

    Create Traversal.cpp

---
+++ 04. Arrays/Traversal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+ int arr[5]={10, 0, 20, 0, 30}; //creating and initializing array    
+        //traversing array    
+       for (int i: arr)     
+        {    
+            cout<<i<<"\n";    
+        }    
+}  
