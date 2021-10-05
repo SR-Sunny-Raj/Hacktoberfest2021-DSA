a52dd15695340984fe0a2dfa8a422228238e0f5f
vishalram14 <89023989+vishalram14@users.noreply.github.com>
2021-10-05 12:49:59 +0530

    Update Maximum_HourglassSum.cpp

--- 07. Matrix/Maximum_HourglassSum.cpp
+++ 07. Matrix/Maximum_HourglassSum.cpp
@@ -9,12 +9,12 @@ Given a  2D Array, :
 0 0 0 0 0 0
 0 0 0 0 0 0
 0 0 0 0 0 0
-An hourglass in  is a subset of values with indices falling in this pattern in 's graphical representation:
+An hourglass is a subset of values with indices falling in this pattern in this graphical representation:
 
 a b c
   d
 e f g
-There are  hourglasses in . An hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+An hourglass sum is the sum of an hourglass' values. Calculate the hourglass sum for every hourglass , then print the maximum hourglass sum.
 
 Example
 
@@ -25,13 +25,15 @@ Example
  0  0  8  6 6 0
  0  0  0 -2 0 0
  0  0  1  2 4 0
+ 
 The  hourglass sums are:
 
 -63, -34, -9, 12, 
 -10,   0, 28, 23, 
 -27, -11, -2, 10, 
   9,  17, 25, 18
-The highest hourglass sum is  from the hourglass beginning at row , column :
+  
+The highest hourglass sum is  from the hourglass beginning at row 1, column 2:
 
 0 4 3
   1
@@ -46,7 +48,13 @@ hourglassSum has the following parameter(s):
 
 int arr[6][6]: an array of integers
 
+Returns
+
+int: the maximum hourglass sum
+
 */
+
+//Solution
 
 int hourglassSum(vector<vector<int>> arr)
 {
