2cff8f65295c44305380cd2cd1062d14e8b71c27
Aradhya Garg <78140726+Aradhyagarg@users.noreply.github.com>
2021-10-06 17:48:42 +0530

    Update infix_to_postfix.cpp

--- 11. Stack/infix_to_postfix.cpp
+++ 11. Stack/infix_to_postfix.cpp
@@ -1,58 +1,66 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<stack>
 using namespace std;
-
-//function to return precedence of operators
-int prec(char c) {
-	if(c == '^')
-		return 3;
-	else if(c == '/' || c=='*')
-		return 2;
-	else if(c == '+' || c == '-')
-		return 1;
-	else
-		return -1;
+int isOperand(char ch) {
+    return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (isdigit(ch));
 }
 
-//required function
-void infixToPostfix(string s) {
+int checkPrecedence(char ch) {
+    switch (ch) {
+    case '+':
+    case '-':
+        return 1;
+    case '*':
+    case '/':
+        return 2;
+    case '^':
+        return 3;
+    }
+    return -1;
+}
 
-	stack<char> st;
-	string result;
-//According to convention of stack implentation of infix to postfix expression, conditions are checked.
-	for(int i = 0; i < s.length(); i++) {
-		char c = s[i];
-		if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
-			result += c;
-		else if(c == '(')
-			st.push('(');
-		else if(c == ')') {
-			while(st.top() != '(')
-			{
-				result += st.top();
-				st.pop();
-			}
-			st.pop();
-		}
-		else {
-			while(!st.empty() && prec(s[i]) <= prec(st.top())) {
-				result += st.top();
-				st.pop();
-			}
-			st.push(c);
-		}
-	}
+int infixToPostfix(char exp[], char output[]) {
+    int i = 0, k = 0;
+    stack < char > st;
+    while (exp[i]) {
+        if (isOperand(exp[i]))
+            output[k++] = exp[i];
+        else if (exp[i] == '(')
+            st.push(exp[i]);
+        else if (exp[i] == ')') {
+            while (!st.empty() && st.top() != '(') {
+                output[k++] = st.top();
+                st.pop();
+            }
+            if (!st.empty() && st.top() != '(')
+                return -1;
+            else
+                st.pop();
+        } else {
+            while (!st.empty() && checkPrecedence(exp[i]) <= checkPrecedence(st.top())) {
+                output[k++] = st.top();
+                st.pop();
+            }
+            st.push(exp[i]);
+        }
+        i++;
+    }
 
-	while(!st.empty()) {
-		result += st.top();
-		st.pop();
-	}
-//Printing the result
-	cout << result << endl;
+    while (!st.empty()) {
+        output[k++] = st.top();
+        st.pop();
+    }
+
+    output[k++] = '\0';
 }
 
 int main() {
-	string exp;
-    cin>>exp;
-	infixToPostfix(exp);
-	return 0;
+    char exp[] = "6/(3^2)-8";
+    char output[20];
+    infixToPostfix(exp, output);
+    cout << "Given infix expression is : " << exp << endl;
+    cout << "Equivalent postfix expression is : " << output << endl;
+    return 0;
 }
