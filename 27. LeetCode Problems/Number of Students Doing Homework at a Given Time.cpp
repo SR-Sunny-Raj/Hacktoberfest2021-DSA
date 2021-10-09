c75e6d3944030f9ace3e59cac8b9a4ee2464602a
Saurav-IIITU <91366385+Saurav-IIITU@users.noreply.github.com>
2021-10-09 09:14:51 +0530

    Update Number of Students Doing Homework at a Given Time.cpp

--- 27. LeetCode Problems/Number of Students Doing Homework at a Given Time.cpp
+++ 27. LeetCode Problems/Number of Students Doing Homework at a Given Time.cpp
@@ -1,3 +1,7 @@
+// Difficulty : Easy
+// https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
+
+
 class Solution {
 public:
     int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
