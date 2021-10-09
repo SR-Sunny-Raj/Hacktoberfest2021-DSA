22d9453e5673a9d7eb0599168dd068b4c02df66a
Satish Kollu <48837350+satishkollu@users.noreply.github.com>
2021-10-09 11:23:41 +0530

    Updated Prime Number Program
    I have added new program to print reason as well.

--- 33. Python Programs/prime_number.py
+++ 33. Python Programs/prime_number.py
@@ -1,10 +1,11 @@
 num = int(input("Enter an integer: "))
-isprime = 1 
-for i in range(2,num//2):
-    if (num%i==0):
-        isprime = 0
-        break
-if(isprime==1):
-    print(num, "is a prime number")
+if num > 1:
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
 else:
-    print(num, "is not a prime number")
+   print(num,"is not a prime number")
