9f95d9fa4b1b9e28389a61ab42c05c58b61a4a90
Asifa23 <73306679+Asifa23@users.noreply.github.com>
2021-10-12 20:54:51 +0530

    Create Cyclically rotate an array by one.cpp

---
+++ Cyclically rotate an array by one.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+void rotate(int arr[], int n);
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n] , i;
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        rotate(a, n);
+        for (i = 0; i < n; i++)
+            printf("%d ", a[i]);
+        printf("\n");
+    }
+        return 0;
+}
+// } Driver Code Ends
+void rotate(int arr[], int n)
+{
+    int temp = arr[n-1];
+    for(int i=n-2;i>=0;i--){
+        arr[i+1]=arr[i];
+        }
+    arr[0]=temp;
+}
