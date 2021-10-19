51d4132131d15372966bab438aa427d2e9030bf1
Rohan Singh Rathore <rohansinghrathore00@gmail.com>
2021-10-19 10:01:09 +0530

    Create highest_occuring_char.cpp

---
+++ highest_occuring_char.cpp
@@ -0,0 +1,38 @@
+// C++ program to output the maximum occurring character
+// in a string
+#include<bits/stdc++.h>
+#define ASCII_SIZE 256
+using namespace std;
+
+char getMaxOccuringChar(char* str)
+{
+	// Create array to keep the count of individual
+	// characters and initialize the array as 0
+	int count[ASCII_SIZE] = {0};
+
+	// Construct character count array from the input
+	// string.
+	int len = strlen(str);
+	int max = 0; // Initialize max count
+	char result; // Initialize result
+
+	// Traversing through the string and maintaining
+	// the count of each character
+	for (int i = 0; i < len; i++) {
+		count[str[i]]++;
+		if (max < count[str[i]]) {
+			max = count[str[i]];
+			result = str[i];
+		}
+	}
+
+	return result;
+}
+
+// Driver program to test the above function
+int main()
+{
+	char str[] = "sample string";
+	cout << "Max occurring character is "
+		<< getMaxOccuringChar(str);
+}
