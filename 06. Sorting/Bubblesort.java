11599d23cbd66e9d2e7f213ace284ec24ab925bf
shuddhi-jain <92723741+shuddhi-jain@users.noreply.github.com>
2021-10-19 23:25:18 +0530

    Update Bubblesort.java

--- 06. Sorting/Bubblesort.java
+++ 06. Sorting/Bubblesort.java
@@ -2,7 +2,17 @@ import java.io.*;
 import java.util.*;
 class BubbleSort{
     
-    static void bubbleSort(int arr[], int n){
+   
+    
+    public static void main (String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n,j;
+        System.out.println("Enter size of array");
+        n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+      
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n - i - 1; j++){
                 if( arr[j] > arr[j + 1]){
@@ -15,20 +25,6 @@ class BubbleSort{
                 }
             }
         }
-    }
-    
-    public static void main (String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n;
-        System.out.println("Enter size of array");
-        n=sc.nextInt();
-        int a[]=new int[n];
-        for(int i=0;i<n;i++){
-            a[i]=sc.nextInt();
-        }
-        
-        bubbleSort(a, n);
-        
         for(int i = 0; i < n; i++){
             System.out.print(a[i] + " ");
         }
