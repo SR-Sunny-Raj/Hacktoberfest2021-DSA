59415b6d756e245201cd0ad2efe474f34211b8b1
Jafar-Sadiq Shittu <62627691+Zuko-J@users.noreply.github.com>
2021-10-21 18:34:38 +0100

    Update FactorialOfNumbers.py

--- 33. Python Programs/FactorialOfNumbers.py
+++ 33. Python Programs/FactorialOfNumbers.py
@@ -4,3 +4,11 @@ for i in range(int(input())):
     for j in range(1,a+1,1):
         fact=fact*j
     print(fact)
+    
+ def factorial(n):
+     
+    return 1 if (n==1 or n==0) else n * factorial(n - 1);
+ 
+num = int(input('Enter number'))
+print("Factorial of",num,"is",
+factorial(num))
