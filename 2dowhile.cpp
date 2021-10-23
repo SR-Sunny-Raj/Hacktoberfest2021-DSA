e417d29a26513478a6eb7b8701374afb05e992b5
Satish Mane <86673164+Satishmane-mit@users.noreply.github.com>
2021-10-23 18:08:09 +0530

    Add files via upload
    this is just a simple do while loop file in c++.

---
+++ 2dowhile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+   do{
+        cout<<n<<endl;
+        cin>>n;
+    }
+    while(n>0);
+    cout<<"Enter a valid Positive Number.....\n";
+}
