c9790f225a5900ce1cf3f25a63572f85f90a75cb
mahalakshmi-08 <71280179+mahalakshmi-08@users.noreply.github.com>
2021-10-29 20:44:20 +0530

    Create Resolution_image.py

---
+++ Resolution_image.py
@@ -0,0 +1,20 @@
+with open(filename,'rb') as img_file:
+
+       # height of image (in 2 bytes) is at 164th position
+       img_file.seek(163)
+
+       # read the 2 bytes
+       a = img_file.read(2)
+
+       # calculate height
+       height = (a[0] << 8) + a[1]
+
+       # next 2 bytes is width
+       a = img_file.read(2)
+
+       # calculate width
+       width = (a[0] << 8) + a[1]
+
+   print("The resolution of the image is",width,"x",height)
+
+jpeg_res("img1.jpg")
