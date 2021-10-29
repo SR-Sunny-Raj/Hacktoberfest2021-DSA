51690aec42156358062814c0e29153212e42ced3
Diptamoy Mitra <91617575+Diptamoy-Mitra@users.noreply.github.com>
2021-10-30 03:51:49 +0530

    Update Valid Parentheses.cpp

--- 09. String/Valid Parentheses.cpp
+++ 09. String/Valid Parentheses.cpp
@@ -17,14 +17,14 @@ s consists of parentheses only '()[]{}'.
 
 class Solution {
 public:
-    bool isValid(string s) {
+    bool isValid(string s2) {
         stack<char> s1;
         
-        for(int i = 0; i < s.size(); i++) {
+        for(int i = 0; i < s2.size(); i++) {
             if(s1.empty()) {
                 s1.push(s[i]);
             } else {
-                if(s1.top() == '(' && s[i] == ')' || s1.top() == '{' && s[i] == '}' || s1.top() == '[' && s[i] == ']') {
+                if(s1.top() == '(' && s2[i] == ')' || s1.top() == '{' && s2[i] == '}' || s1.top() == '[' && s2[i] == ']') {
                 s1.pop();
             } else {
                 s1.push(s[i]);
