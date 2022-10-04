3fb5259a12d443801746a4aeaf795964d9e135e1
fabProMe <113090237+fabProMe@users.noreply.github.com>
2022-10-04 21:58:49 +0530

    added Find_Missing_And_Repeating.cpp

---
+++ 04. Arrays/Find_Missing_And_Repeating.cpp
@@ -0,0 +1,32 @@
+// C++ program to Find the repeating
+// and missing elements
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printTwoElements(int arr[], int size)
+{
+    int i;
+    cout << "The repeating element is ";
+
+    for (i = 0; i < size; i++) {
+        if (arr[abs(arr[i]) - 1] > 0)
+            arr[abs(arr[i]) - 1] = -arr[abs(arr[i]) - 1];
+        else
+            cout << abs(arr[i]) << "\n";
+    }
+
+    cout << "and the missing element is ";
+    for (i = 0; i < size; i++) {
+        if (arr[i] > 0)
+            cout << (i + 1);
+    }
+}
+
+/* Driver code */
+int main()
+{
+    int arr[] = { 7, 3, 4, 5, 5, 6, 2 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printTwoElements(arr, n);
+}
