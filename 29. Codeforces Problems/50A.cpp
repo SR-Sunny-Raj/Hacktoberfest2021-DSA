5ca8fcfb3ab8d9c1399d5cfecfcdbe5d7514b5d8
anishnitjsr <101205000+anishnitjsr@users.noreply.github.com>
2022-10-06 11:34:04 +0530

    Update 50A.cpp

--- 29. Codeforces Problems/50A.cpp
+++ 29. Codeforces Problems/50A.cpp
@@ -1,14 +1,13 @@
 // Link to problem: https://codeforces.com/problemset/problem/50/A
 
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+
 using namespace std;
+
 int main()
 {
-    int a, b, n, m;
-    cin>>a>>b;
-    n = ( int(min(a,b)/2) * (max(a,b)) );
-    m = (a*b) - (2*n);
-    m = m/2;
-    cout<<n + m<<endl;
+    int M, N;
+    cin >> M >> N;
+    cout << M * N / 2 << endl;
+    return 0;
 }
