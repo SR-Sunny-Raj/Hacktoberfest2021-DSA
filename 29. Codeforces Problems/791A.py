6ca9032e49ec8b2039ca295e4fe52f6c4c806b64
anishnitjsr <101205000+anishnitjsr@users.noreply.github.com>
2022-10-06 11:26:27 +0530

    Update 791A.py

--- 29. Codeforces Problems/791A.py
+++ 29. Codeforces Problems/791A.py
@@ -1,12 +1,21 @@
 # https://codeforces.com/problemset/problem/791/A
 # 791A - Bear and Big Brother
-
-sizes = [int(size) for size in input().split()]
-i = 0
-
-while sizes[0] <= sizes[1]:
-    sizes[0] *= 3
-    sizes[1] *= 2
-    i += 1
-
-print(i)
+#this code is written in c++ langaunge
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c=0;
+    cin>>a>>b;
+    if(a>b)
+        c=0;
+    else
+        while(a<=b)
+        {
+            a=a*3;
+            b=b*2;
+            c++;
+        }
+    cout<<c<<endl;
+    return 0;
+}
